5cb980c27e507c88849ff6688c3237047938543a
Dongjin113 <104759062+Dongjin113@users.noreply.github.com>
2024-04-02 17:51:22 +0900

    ✨ MainPage (#3)
    * ✨ MainPage Nav바 추가
    
    * ✨ MainPage ContentBox의 ContentItem 컴포넌트 추가
    
    * ✨ MainPage ContentBox 컴포넌트 추가
    
    * ✨ MainPage SelectTag 컴포넌트 추가
    
    * ✨ 카카오 맵 컴포넌트 추가
    
    * ✨ 내 위치를 가져오는 CustomHook 추가
    
    * ✨ 카카오 맵위의 캠핑장 정보 Overlay Recoil을 사용하여 전역으로 관리
    
    * ✨ MainPage 요청 API 및 응답데이터 타입 정의
    
    * ✨ footer 컴포넌트 추가
    
    * ✨ MainPage 정적데이터 API 응답 데이터로 변경
    
    * ✨ 환경변수로 KAKAO MAP KEY 받아오기

--- src/pages/MainPage/MainPage.style.tsx
+++ src/pages/MainPage/MainPage.style.tsx
@@ -1,14 +1,29 @@
 import styled from "styled-components";
+import * as COLOR from "../../constants/color";
+import * as FONT from "../../constants/font";
 
-export const Nav = styled.nav`
-  display: flex;
+export const MainPageContainer = styled.div`
   justify-content: space-between;
-  align-items: top;
 `;
 
-export const Logo = styled.div`
-  img {
-    width: 200px;
-    height: 180px;
+export const ContentBoxContainer = styled.div`
+  padding-top: "20px";
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+
+  @media screen and (max-width: 1000px) {
+    align-items: flex-start;
   }
+`;
+
+export const MapContainer = styled.div`
+  padding-top: ${({ isMenuFixed }) => (isMenuFixed ? "300px" : "20px")};
+
+  z-index: -2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10%;
 `;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,24 +1,146 @@
-import React from "react";
-import { Nav, Logo } from "./MainPage.style";
+import React, { useEffect, useState } from "react";
+import NavBar from "../Nav/Nav";
+import ContentBox from "../../components/ContentBox/ContentBox";
+import {
+  ContentBoxContainer,
+  MainPageContainer,
+  MapContainer,
+} from "./MainPage.style";
+import Footer from "../Footer/Footer";
+import SelectTag from "../../components/SelectTag/SelectTag";
+import MainPageMap from "../../components/Map/MainPageMap";
+import useMainPageAPI from "../../api/camping/useMainPageAPI";
+import { campingPreview } from "../../api/constants/camping";
+import { useRecoilState } from "recoil";
+import { isOpenState } from "../../recoil/recoilAtom";
 
 function MainPage() {
+  const [isMenuFixed, setMenuFixed] = useState(false);
+  const [regionCampings, setRegionCampings] = useState<campingPreview[]>();
+  const { requestPreviewCamping } = useMainPageAPI();
+  const [region, setRegion] = useState();
+  const [isOpen, setIsOpen] = useRecoilState(isOpenState);
+
+  const detailInfoHandler = (campingId) => {
+    setIsOpen((prev) => {
+      const updatedIsOpen = [...prev];
+      updatedIsOpen[campingId] = !prev[campingId];
+      return updatedIsOpen;
+    });
+  };
+
+  const city = [
+    // "서울특별시",
+    // "경기도",
+    // "강원도",
+    // "전라남도",
+    // "전라북도",
+    // "경상남도",
+    // "경상북도",
+    // "충청남도",
+    // "충청북도",
+    // "광주",
+    // "대구",
+    // "대전",
+    // "부산",
+    // "울산",
+    // "세종",
+    // "인천",
+    // "제주",
+
+    "전라북도",
+    "경상북도",
+    "전라남도",
+    "경상남도",
+    "경기도",
+    "강원도",
+    "제주특별자치도",
+    "충청남도",
+    "충청북도",
+    "대구광역시",
+    "서울특별시",
+    "인천광역시",
+    "광주광역시",
+    "대전광역시",
+    "세종특별자치시",
+    "울산광역시",
+    "부산광역시",
+  ];
+
+  const handleRegionCamping = (region) => {
+    setRegion(region);
+    requestPreviewCamping(region, setRegionCampings);
+    console.log(regionCampings);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    handleRegionCamping("서울특별시");
+  }, []);
+
+  const handleScroll = () => {
+    if (window.scrollY > 130) {
+      setMenuFixed(true);
+    } else {
+      setMenuFixed(false);
+    }
+  };
+
+  const cityRows = [];
+  const maxCitiesPerRow = 9;
+
+  // city 배열을 7개씩 나누어서 cityRows에 추가
+  for (let i = 0; i < city.length; i += maxCitiesPerRow) {
+    cityRows.push(city.slice(i, i + maxCitiesPerRow));
+  }
+
   return (
-    <Nav>
-      <Logo>
-        <img src="/images/Some.png" alt="로고" />
-      </Logo>
-      <div className="menu">
-        <ul className="menu items">
-          <li className="menu item">관광지</li>
-          <li className="menu item">맛집</li>
-          <li className="menu item">카페</li>
-          <li className="menu item">캠핑</li>
-        </ul>
-      </div>
-      <div>
-        <input type="text" placeholder="검색어를 입력해주세요" />
-      </div>
-    </Nav>
+    <MainPageContainer>
+      <NavBar />
+      <SelectTag
+        cityRows={cityRows}
+        isMenuFixed={isMenuFixed}
+        requestRegionCamping={handleRegionCamping}
+      />
+      <MapContainer isMenuFixed={isMenuFixed}>
+        {regionCampings && (
+          <MainPageMap
+            campingData={regionCampings}
+            detailInfoHandler={detailInfoHandler}
+          />
+        )}
+      </MapContainer>
+      <ContentBoxContainer isMenuFixed={isMenuFixed}>
+        {regionCampings && (
+          <ContentBox
+            detailInfoHandler={detailInfoHandler}
+            region={region}
+            category={"캠핑장"}
+            campingData={regionCampings}
+          />
+        )}
+        {regionCampings && (
+          <ContentBox region={region} campingData={regionCampings} />
+        )}
+
+        {regionCampings && (
+          <ContentBox region={region} campingData={regionCampings} />
+        )}
+        {regionCampings && (
+          <ContentBox region={region} campingData={regionCampings} />
+        )}
+        {regionCampings && (
+          <ContentBox region={region} campingData={regionCampings} />
+        )}
+      </ContentBoxContainer>
+      <Footer />
+    </MainPageContainer>
   );
 }
 
