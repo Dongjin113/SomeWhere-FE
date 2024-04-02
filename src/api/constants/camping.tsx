2f316eb2535591139598bac1395d40c6b215cea8
DongJinLee <dlehdwls21@naver.com>
2024-04-02 15:45:50 +0900

    ✨ MainPage 요청 API 및 응답데이터 타입 정의

---
+++ src/api/constants/camping.tsx
@@ -0,0 +1,11 @@
+import { address, location } from "./common";
+
+export interface campingPreview {
+  campingId: number;
+  placeName: string;
+  address: address;
+  location: location;
+  phoneNum: string;
+  homePage: string;
+  subIntro: string;
+}
